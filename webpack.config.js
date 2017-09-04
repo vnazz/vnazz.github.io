e4425041c7ccf8da93911a1a3e3c4fef6907d561
Victoria Nazari <victorianazari@gmail.com>
2017-09-04 18:27:30 -0500

    Redesign with react

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var APP_DIR = path.resolve(__dirname, 'src/components');
+var BUILD_DIR = path.resolve(__dirname, 'src/app');
+
+module.exports = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'index.js'
+     },
+    module : {
+        loaders : [
+          {
+            test : /\.jsx?/,
+            include : APP_DIR,
+            loader : 'babel-loader'
+          }
+        ]
+    }
+};
